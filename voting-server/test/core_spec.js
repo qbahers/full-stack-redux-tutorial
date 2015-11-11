0a80d8d49f4974f88c24293d3becfb7e73ebf453
Quentin Bahers <quentin.bahers@gmail.com>
2015-11-11 16:33:26 +0000

    Starting The Vote

--- voting-server/test/core_spec.js
+++ voting-server/test/core_spec.js
@@ -1,7 +1,7 @@
 import {List, Map} from 'immutable';
 import {expect} from 'chai';
 
-import {setEntries} from '../src/core';
+import {setEntries, next} from '../src/core';
 
 describe('application logic', () => {
 
@@ -25,6 +25,19 @@ describe('application logic', () => {
       }));
     });
 
+  });
+
+  describe('next', () => {
+    const state = Map({
+      entries: List.of('Trainspotting', '28 Days Later', 'Sunshine')
+    });
+    const nextState = next(state);
+    expect(nextState).to.equal(Map({
+      vote: Map({
+        pair: List.of('Trainspotting', '28 Days Later')
+      }),
+      entries: List.of('Sunshine')
+    }));
   });
 
 });
